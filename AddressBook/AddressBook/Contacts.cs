11a3976d708c6fdb3885569f37fd7d024834717e
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-20 16:56:40 +0530

    UC1-Creat a Contact

---
+++ AddressBook/AddressBook/Contacts.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class Contacts
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string phonenumber { get; set; }
+        string city { get; set; }
+        string state { get; set; }
+        string Zip { get; set; }
+        string address { get; set; }
+
+        public List<Contacts> People = new List<Contacts>();
+        public void addperson()
+        {
+
+            Contacts person = new Contacts();
+
+
+            Console.WriteLine("Add a entry to your Address book");
+
+
+            Console.Write("Enter First Name: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            person.phonenumber = Console.ReadLine();
+
+            Console.Write("Enter Address 1: ");
+
+            person.address = Console.ReadLine();
+            Console.Write("Enter Zip: ");
+            person.Zip = Console.ReadLine();
+
+            Console.Write("Enter city: ");
+            person.city = Console.ReadLine();
+
+            Console.Write("Enter state: ");
+            person.state = Console.ReadLine();
+
+
+            People.Add(person);
+        }
+    }
+}
