ea1345bce14838fe6d7b6d00f3f4edc48005c798
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-21 18:50:25 +0530

    UC2-AddContactDetails

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -9,10 +9,29 @@ namespace AddressBook
             Console.WriteLine("Welcome to Address Book Program");
 
             Console.WriteLine("-----------------------");
+            Console.WriteLine("--Add Contact Details--");
 
-            ContactOperation contactOperation = new ContactOperation();
-            contactOperation.addContact("Vaibhav", "Manapure", "At.Po.Ta Samudrapur,Wardha", "Nagpur", "Maharashtra", "442305", "9764510067", "manapure.vaibhav542@gmail.com");
-            contactOperation.showList();
+            Console.WriteLine("First Name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Last Name:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Address:");
+            string address = Console.ReadLine();
+            Console.WriteLine("City:");
+            string city = Console.ReadLine();
+            Console.WriteLine("State:");
+            string state = Console.ReadLine();
+            Console.WriteLine("Zip Code:");
+            string zip = Console.ReadLine();
+            Console.WriteLine("Phone Number:");
+            string phoneNumber = Console.ReadLine();
+            Console.WriteLine("Email Address:");
+            string email = Console.ReadLine();
+
+
+            //ContactOperation contactOperation = new ContactOperation();
+            //contactOperation.addContact("Vaibhav", "Manapure", "At.Po.Ta Samudrapur,Wardha", "Nagpur", "Maharashtra", "442305", "9764510067", "manapure.vaibhav542@gmail.com");
+            //contactOperation.showList();
 
             Console.ReadKey();
         }
